00c5e8391f4c441a89a722fcd5cfeb1af8941607
Ian Gaither <ianrgaither@gmail.com>
2023-08-04 20:02:20 -0700

    Initial text content for three projects
    Revised the project structure to better accommodate different information for each project.
    Added text content for each project describing the project itself and my role within that project.
    Updated project-entry to display and use all the text fields in the project interface.

--- src/app/interfaces/project.ts
+++ src/app/interfaces/project.ts
@@ -1,7 +1,18 @@
+export enum MediaType {
+	Video,
+	Image
+}
+
+export interface TextLink {
+	text: string;
+	link: string;
+}
+
 export interface Project {
 	title: string;
 	summary: string;
 	role: string;
-	references: {text: string, link: string | null | undefined}[] | null;
-	media: string | null;
+	downloads?: TextLink[];
+	references?: {description: string, textLink: TextLink}[];
+	media?: {filename: string, type: MediaType}[];
 };
--- src/app/services/project-data-memory-store.service.ts
+++ src/app/services/project-data-memory-store.service.ts
@@ -5,25 +5,31 @@ import { Project } from '../interfaces/project';
 
 const projectList: Project[] = [
 	{
-		title: 'Test Project 1',
-		summary: 'Test Summary AAAAAAAAAAAAAAAAAAAAAAAAAAAAAA',
-		role: 'Test Role AAAAAAAAAAAAAAAAAAAAAAAAAAAAAA',
-		references: [
-			{ text: 'Downloads', link: null },
-			{ text: 'Google', link: 'https://www.google.com' },
-			{ text: 'Youtube', link: 'https://www.youtube.com' },
-		],
-		media: null
+		title: 'Lirica',
+		summary: 'Lirica was an art centric MFA (Master of Fine Arts) thesis project built over nine months using Unreal Engine 4. This project had a large multidisciplinary team with upwards of 30 people at its largest and had core themes focusing on the integration of music into gameplay and visuals.',
+		role: 'My primary roles on this team included the integration of animations and gameplay logic, as well as AI behaviors and interactions for all the creatures throughout the world of Lirica. This involved the creation of several custom animations and sequences for mechanics that responded to the player’s actions. However, as a tech lead for the project I was also involved in planning, organizing, and developing many of the low level systems within the game.',
+		downloads: [
+			{ text: 'Steam', link: 'https://store.steampowered.com/app/1733460/Lirica' },
+			{ text: 'DigiPen Institute of Technology', link: 'https://games.digipen.edu/games/lirica' }
+		]
 	},
 	{
-		title: 'Test Project 2',
-		summary: 'Test Summary BBBBBBBBBBBBBBBBBBBBBBBBB',
-		role: 'Test Role BBBBBBBBBBBBBBBBBBBBBBBBB',
+		title: 'Ascent',
+		summary: 'Ascent was a nine month project with a dual focus– first, to create a custom game engine from scratch using C++, then to develop a game using that engine. Our multidisciplinary team of ten settled on a 2D puzzle platformer that utilized transformation abilities to interact with the world.',
+		role: 'My role for this project began with developing the engine’s editor, a tool that our artists and designers could use to build the game and world without having to directly interact with the codebase. However, as we pivoted from developing the engine to focusing on the game itself, my role became much more versatile. I developed most of the game\'s base mechanics, rebuilt the player behavior and controls, and even improved many low level engine systems including physics, collision, and asset loading.',
+		downloads: [
+			{ text: 'DigiPen Institute of Technology', link: 'https://games.digipen.edu/games/ascent' }
+		]
+	},
+	{
+		title: 'TD Squared',
+		summary: 'TD Squared was my capstone project at Oregon State University and a project concept I initially devised and proposed myself. I was able to recruit three other computer science majors for the nine month project, which was built entirely in Unreal Engine 4. The base concept was a unique combination of a top-down shooter and a tower defense, providing many different opportunities for expanding upon the mechanics of both genres as they mixed together.',
+		role: 'For this project I acted as both the Technical Lead and the Project Manager for the team, handling meetings, documentation, and setting goals and milestones for the project as a whole. For the game itself I focused mainly on lower level systems and aspects that required a deeper knowledge of game development, as I was the only one on the team with previous game development experience. This included tasks like setting up collision channels, creating the player controller, and setting up the AI pathfinding system. I also focused heavily on creating base classes and building blocks that enabled us to rapidly create new abilities and towers without having to worry about how those new entities interacted with the existing game infrastructure.',
 		references: [
-			{ text: 'Downloads', link: null },
-			{ text: 'Stack Overflow', link: 'https://stackoverflow.com' },
-		],
-		media: null
+			{
+			description: 'During this project I also wrote a series of blog posts– one of which highlighted the randomized shop system and how I designed the shop’s tower selection process in order to reduce the complexity of creating new towers for the game. This post can be found here:',
+			textLink: { text: 'Developing Randomness in our Game: Dynamic Scaling and Relative Probability', link: 'https://blogs.oregonstate.edu/iangaither/2021/11/19/developing-randomness-in-our-game-dynamic-scaling-and-relative-probability/'}
+		}]
 	}
 ]
 
